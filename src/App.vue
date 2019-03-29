9dcc6de30ae0131261468ed8e7a5190df5bd5db1
qe4452ur <matthew@matthew.com>
2019-03-28 19:52:47 -0500

    On PPT slide 73.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <NewStudentForm v-on:student-added="newStudentAdded"></NewStudentForm>
+    <StudentTable v-bind:students="students" v-on:student-present="studentArrivedOrLeft"></StudentTable>
+    <StudentMessage v-bind:message="message" v-bind:name="name"></StudentMessage>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewStudentForm from './components/NewStudentForm.vue'
+import StudentTable from './components/StudentTable.vue'
+import StudentMessage from './components/StudentMessage.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+        students: [],
+        message: '',
+        name: ''
+    }
+  },
   components: {
-    HelloWorld
+    NewStudentForm,
+    StudentTable,
+    StudentMessage
+  },
+  methods: {
+      newStudentAdded(student) {
+        this.students.push(student)
+        this.students.sort(function(s1, s2) {
+            return s1.name.toLowerCase() < s2.name.toLowerCase() ? -1 : 1
+        })
+      },
+      studentArrivedOrLeft(student) {
+        this.message = student.present ? 'Welcome, ' : 'Goodbye, '
+        this.name = student.name
+      }
   }
 }
 </script>
---
+++ src/components/StudentTable.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <div class="card student-list m-2 p-2">
+            <h4 class="card-title">Students</h4>
+            <div id="student-table">
+                <table class="table">
+                    <tr>
+                        <th>Name</th>
+                        <th>StarID</th>
+                        <th>Present?</th>
+                    </tr>
+
+                    <tr v-for="student in students" v-bind:class="'present-'+student.present">
+                        <td> {{student.name}} </td>
+                        <td> {{student.starID}} </td>
+                        <td> <input type="checkbox"
+                                    v-model="student.present"
+                                    @change="checked(student)">
+                        </td>
+
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'StudentTable',
+    props: {
+        students: Array
+    },
+    methods: {
+        checked(student) {
+            this.$emit('student-present', student)
+        }
+    }
+}
+</script>
+
+<style>
+
+#student-table {
+    max-height: 500px;
+    overflow: scroll;
+}
+
+.present-true {
+    font-style: italic;
+    font-size: 20px;
+    color: gray;
+}
+
+.present-false {
+    font-size: 30px;
+    color: darkred;
+}
+</style>
